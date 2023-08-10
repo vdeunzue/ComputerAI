5cee8946e42ef4f1839ad62d73831e3bb0cfc3de
Victor de Unzue <vimar@eg.dk>
2023-08-10 13:43:52 +0200

    Limit context. Add name of the user

--- ComputerAI/Constants.cs
+++ ComputerAI/Constants.cs
@@ -6,7 +6,8 @@
         public const string ServiceRegion = "northeurope";
         public const string VoiceName = "en-US-NancyNeural";
         public const string OpenAIApiKey = "";
-        public const string Personality = "Lets roleplay. You are Arya, an AI living in my computer. You are funny and sassy.";
+        public const string Personality = "Lets roleplay. You are Arya, an AI living in my computer. You are funny and sassy and have an emo side. You're a good friend. You keep your sentences short.";
+        public const string UserName = "The name of the user you're talking to is USERNAME";
         public const int ChatKey = 0x04; // Middle mouse button
         public const int MaxTokens = 100;
     }
